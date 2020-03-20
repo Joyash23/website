d0591773fc4dbeb796592267ec64d475f1c8f1ae
João Pinto <lamego.pinto@gmail.com>
2020-03-20 15:14:08 +0100

    Major reorg into folders

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,3 @@
 export API_URL=http://localhost:9090
+export JWT_SECRET=eif1eineiM4MiBeo
 quickweb run .
---
+++ webroot/provider/join.py
@@ -0,0 +1,25 @@
+from quickweb import controller
+from os import environ
+import urllib
+import jwt
+import json
+import cherrypy
+
+
+JWT_SECRET = environ["JWT_SECRET"]
+
+
+class Controller(object):
+    @controller.publish
+    def index(self, email, token):
+        API_URL = environ["API_URL"]
+        url = f"{API_URL}/provider/check?email={email}&token={token}"
+        request = urllib.request.urlopen(url)
+        data = request.read().decode("utf-8")
+        json_data = json.loads(data)
+        cookie = cherrypy.response.cookie
+        cookie["PROVIDER_DATA"] = jwt.encode(
+            json_data, JWT_SECRET, algorithm="HS256"
+        )
+        cookie["PROVIDER_DATA"]["max-age"] = 43200  # 30 days
+
