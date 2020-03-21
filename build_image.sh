756ced9572b921ac18031c6bcaa0f9776b0b2ba4
João Pinto <lamego.pinto@gmail.com>
2020-03-21 14:06:08 +0100

    Add docker image build script

---
+++ build_image.sh
@@ -0,0 +1,2 @@
+docker build -t registry.appuio.ch/sb-viralhelp/website:latest  .
+docker push registry.appuio.ch/sb-viralhelp/website:latest
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,2 @@
 export API_URL=http://localhost:9090
-export JWT_SECRET=eif1eineiM4MiBeo
 quickweb run .
