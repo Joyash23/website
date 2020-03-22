34a2d921ae87909480550296c6dcb6ea7ec0b744
oanaapostol <oana.apostol85@gmail.com>
2020-03-22 11:33:38 +0100

    Fix spoken languages

--- api/webroot/provider/profile.py
+++ api/webroot/provider/profile.py
@@ -5,10 +5,16 @@ class Controller(object):
     @controller.publish
     def index(self, zipcode, **kwargs):
         email = kwargs.get("email")
-        languages = "|".join(kwargs.get("language", []))
-        category = "|".join(kwargs.get("category", []))
+        language = kwargs.get("language", [])
+        category = kwargs.get("category", [])
         notify = kwargs.get("notify")
 
+        if type(language) in (tuple, list):
+            language = "|".join(language)
+
+        if type(category) in (tuple, list):
+            category = "|".join(category)
+
         # Read email from user session
-        controller.lib.db.update_provider(email, zipcode, languages, category, notify)
+        controller.lib.db.update_provider(email, zipcode, language, category, notify)
         return "OK"
