98b343009fb69fb0bc5acf917e409678b6f9aed3
João Pinto <lamego.pinto@gmail.com>
2020-03-24 11:25:35 +0100

    Fix help requests count

--- api/webroot/provider/stats.py
+++ api/webroot/provider/stats.py
@@ -8,7 +8,10 @@ class Controller(object):
     @cherrypy.tools.json_out()
     def default(self, *args, **kwargs):
         providers_count = controller.lib.db.get_providers_count()[0]
-        requests_count = controller.lib.db.get_help_requests_count()[0]
-        return json.dumps(
-            {"providers_count": providers_count, "requests_count": requests_count}
-        )
+        help_requests_count = controller.lib.db.get_help_requests_count()[0]
+        data = {
+            "providers_count": providers_count,
+            "help_requests_count": help_requests_count,
+        }
+        print(data)
+        return json.dumps(data)
